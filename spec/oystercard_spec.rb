963a3c8f454e0e4e63076b7b5fc25eb5e1cb6a1a
Lucy Matejka <lucymatejka@yahoo.co.uk>
2019-11-05 17:24:20 +0000

    complete challenge #4: create unit tests for balance

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,3 +1,18 @@
+require 'oystercard'
+
 describe Oystercard do
-  
+  it 'creates an instance of Oystercard' do
+    oystercard = Oystercard.new
+    expect(oystercard).to be_an_instance_of(Oystercard)
+  end
+
+  # it 'responds to balance' do
+  #   oystercard = Oystercard.new
+  #   expect(oystercard).to respond_to(:balance)
+  # end
+
+  it 'should show an initial balance of zero' do
+    oystercard = Oystercard.new
+    expect(oystercard.balance).to eq(0)
+  end
 end
