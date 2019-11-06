35db347d29e722cdc7d4f4f5d9d417cf82c8a672
Sonia Quintiao <soniaquintiao@gmail.com>
2019-11-06 14:35:07 +0000

    complete tet for top_up method

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -1,18 +1,19 @@
 require 'oystercard'
 
 describe Oystercard do
-  it 'creates an instance of Oystercard' do
-    oystercard = Oystercard.new
-    expect(oystercard).to be_an_instance_of(Oystercard)
-  end
 
-  # it 'responds to balance' do
+  it {is_expected.to be_an_instance_of(Oystercard)}
+
+  # unit test below can be refactored out as later tests build on this method
+  # it 'should respond to balance' do
   #   oystercard = Oystercard.new
   #   expect(oystercard).to respond_to(:balance)
   # end
 
   it 'should show an initial balance of zero' do
-    oystercard = Oystercard.new
-    expect(oystercard.balance).to eq(0)
+    expect(subject.balance).to eq(0)
   end
+
+  it {is_expected.to respond_to(top_up).with(1).argument}
+
 end
