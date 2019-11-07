d3b828a959a8becbeff6ed0012589be728cb35e0
Sonia Quintiao <soniaquintiao@gmail.com>
2019-11-07 16:50:17 +0000

    Completes #7 adding code to pass test

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -8,9 +8,12 @@ class Oystercard
   end
   def top_up(amount)
     # 'balance' does not need to be @balance in below line because the attr_reader
-    # allows the specific instance of balance to be called, so @balance doesn't need to be repated
+    # allows the specific instance of balance to be called, so @balance doesn't need to be repeated
     fail "Maximum balance #{MAXIMUM_LIMIT} exceeded" if balance + amount > MAXIMUM_LIMIT
     @balance += amount
+  end
+  def deduct(amount)
+    @balance -= amount
   end
 
 end
