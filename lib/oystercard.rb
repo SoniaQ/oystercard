bb603f6ceb955ac583cce4d7b77389841a182b1a
Sonia Quintiao <soniaquintiao@gmail.com>
2019-11-11 15:54:26 +0000

    Adds application code to pass unit test for touch_out method for #8

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -7,17 +7,23 @@ class Oystercard
   def initialize
     @balance = 0
   end
+  
   def top_up(amount)
     # 'balance' does not need to be @balance in below line because the attr_reader
     # allows the specific instance of balance to be called, so @balance doesn't need to be repeated
     fail "Maximum balance #{MAX_BALANCE} exceeded" if balance + amount > MAX_BALANCE
     @balance += amount
   end
+
   def deduct(amount)
     fail "Minimum balance of £1 required" if balance - amount < MIN_BALANCE
     @balance -= amount
   end
+
   def touch_in
+  end
+
+  def touch_out
   end
 
 end
