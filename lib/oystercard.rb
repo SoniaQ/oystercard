6de11e240f54a77b85f0b798fa06e71b7dc558b1
Sonia Quintiao <soniaquintiao@gmail.com>
2019-11-11 16:25:38 +0000

    Adds application code to pass unit tests for @in_journey for #8

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -2,10 +2,11 @@ class Oystercard
 
   MAX_BALANCE = 90
   MIN_BALANCE = 1
-  attr_reader :balance
+  attr_reader :balance, :in_journey
 
   def initialize
     @balance = 0
+    @in_journey = false
   end
 
   def top_up(amount)
@@ -21,12 +22,11 @@ class Oystercard
   end
 
   def touch_in
+    @in_journey = true
   end
 
   def touch_out
-  end
-
-  def in_journey?
+    @in_journey = false
   end
 
 end
